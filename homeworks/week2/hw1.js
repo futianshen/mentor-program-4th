5bf09f262edef37aa38670c844376de2852f5fd2
FutianShen <pcsh810778@gmail.com>
2020-07-18 12:13:55 +0800

    [HOMEWORK] week2 hw1 printStars

--- homeworks/week2/hw1.js
+++ homeworks/week2/hw1.js
@@ -1,5 +1,27 @@
 function printStars(n) {
-  
+  if (typeof n !== 'number') {
+    const errMsg = '"n" should be a number'
+    console.log(errMsg)
+    return Error(errMsg)
+  }
+  if (n % 1) {
+    const errMsg = '"n" should be an integer'
+    console.log(errMsg)
+    return Error(errMsg)
+  }
+  if (n < 1 || n > 30) {
+    const errMsg = '"n" should in 1 to 30'
+    console.log(errMsg)
+    return Error(errMsg)
+  }
+  for (let i = 0; i < n; i += 1) {
+    console.log('*')
+  }
+  return 'Completed'
 }
 
-printStars(5);
+printStars('1234')
+printStars(0)
+printStars(5)
+printStars(6.6)
+printStars(31)
